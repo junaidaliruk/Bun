fd44852064c75402ef22f2536b5f06be098af3b1
junaidaliruk <junaidruk526@gmail.com>
2026-09-20 00:07:59 -0700

    commt

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { serve } from "bun";
 import index from "./index.html";
-
-export const server = serve({
+const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
