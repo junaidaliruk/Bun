cfed9aa045640c616684a0f57c1ae5d28a58512a
junaidaliruk <junaidruk526@gmail.com>
2026-09-20 00:20:13 -0700

    commt

--- api/hello/[name].ts
+++ api/hello/[name].ts
@@ -1,7 +1,9 @@
 export default async function handler(req: Request): Promise<Response> {
-  const url = new URL(req.url);
+  // `req.url` may be absolute or relative depending on the deployment runtime,
+  // so provide a base to guarantee `new URL(...)` never throws.
+  const url = new URL(req.url, "http://localhost");
   const segments = url.pathname.split("/").filter(Boolean);
-  const name = segments[segments.length - 1] ?? "world";
+  const name = decodeURIComponent(segments[segments.length - 1] ?? "world");
 
   return Response.json({
     message: `Hello, ${name}!`,
